cb4fa7a58c53fa4a57139806e556f5a6eb61dcaa
orznijiang <527816543@qq.com>
2022-04-30 21:44:50 +0800

    C++ Prime Plus : add : exercise 08-01(not completely achieved)

---
+++ C++ Primer Plus/Programming Exercise/Exercise/Chapter_8/08-01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void print_str(const char*, int n = 0);
+
+int main() {
+	const char* str = "hello";
+	print_str(str);
+	cout << endl;
+	print_str(str, 0);
+	cout << endl;
+	print_str(str, 1);
+
+	return 0;
+}
+
+void print_str(const char* str, int n) {
+	static int flag = 0;
+	flag++;
+	if (n) {
+		for (int i = 0; i < flag; i++) {
+			cout << str << endl;
+		}
+	}
+	else {
+		cout << str << endl;
+	}
+}
